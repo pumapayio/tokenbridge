86f7c1f25c96163f8ec2ff4c80e986f87fc41c11
Ankit Bajaj <abajaj@deqode.com>
2021-05-20 12:46:33 +0530

    DV-705 fix comma bug

--- ui/src/components/Bridge.js
+++ ui/src/components/Bridge.js
@@ -183,8 +183,8 @@ export class Bridge extends React.Component {
   handleSendMax = balance => {
 
     const formattedBalance = isNaN(numeral(balance).format('0.00', Math.floor))
-    ? numeral(0).format('0,0.00', Math.floor)
-    : numeral(balance).format('0,0.00', Math.floor)
+    ? numeral(0).format('00.00', Math.floor)
+    : numeral(balance).format('00.00', Math.floor)
 
     this.setState({
       "formattedBalance": formattedBalance,
